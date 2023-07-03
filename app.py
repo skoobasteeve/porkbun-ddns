3040f99b81d23be8fe60356a5e21b59abafc5c54
Ray Lyon <ray@raylyon.net>
2023-07-03 16:30:50 -0400

    update functionality working

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import requests
 import json
+import os
 
 porkbun_url = "https://porkbun.com/api/json/v3"
-api_key = ""
-secret_api_key = ""
+api_key = os.environ.get("PORKBUN_API_KEY")
+secret_api_key = os.environ.get("PORKBUN_SECRET_API_KEY")
 config_file = "config.json"
 
 
@@ -22,7 +23,8 @@ def get_public_ip(url: str, headers: dict, body: dict):
 
 
 def get_records(url: str, headers: dict, body: dict, domain: str):
-    request = requests.post(url=f"{url}/dns/retrieve/{domain}", headers=headers, json=body)
+    request = requests.post(url=f"{url}/dns/retrieve/{domain}", headers=headers,
+                            json=body)
     request.raise_for_status
 
     if request.json()["status"] == "SUCCESS":
@@ -34,15 +36,31 @@ def get_records(url: str, headers: dict, body: dict, domain: str):
     return records
 
 
-def compare_records(domain: str, subdomains: list, current_records: dict, ip: str):
+def compare_records(domain: str, subdomains: list, current_records: dict,
+                    ip: str):
     to_update = []
     for sub in subdomains:
         records = [r for r in current_records if r["name"] == f"{sub}.{domain}"]
         for r in records:
+            record_dict = {}
             if r["content"] != ip:
-                to_update.append(r["name"])
+                record_dict["domain"] = domain
+                record_dict["subdomain"] = sub
+                to_update.append(record_dict)
     
     return to_update
+
+
+def update_record(url: str, headers: dict, body: dict, domain: str,
+                  subdomain: str, ip: str):
+    body["content"] = ip
+    body["ttl"] = "600"
+
+    request = requests.post(url=f"{url}/dns/editByNameType/{domain}/A/{subdomain}",
+                            headers=headers, json=body)
+    request.raise_for_status
+
+    return request.json()["status"]
 
 
 def main():
@@ -57,14 +75,25 @@ def main():
 
     public_ip = get_public_ip(url=porkbun_url, headers=headers, body=body)
 
-    with open(config_file) as f:
+    with open(config_file, 'r') as f:
         config_data = json.load(f)
 
+    records_to_update = []
     for r in config_data["records"]:
-        current_records = get_records(url=porkbun_url, headers=headers, body=body, domain=r["domain"])
-        records_to_update = compare_records(domain=r["domain"], subdomains=r["subdomains"], current_records=current_records, ip=public_ip)
-        print(records_to_update)
-
+        current_records = get_records(url=porkbun_url, headers=headers, body=body,
+                                      domain=r["domain"])
+        r_to_update = compare_records(domain=r["domain"],
+                                            subdomains=r["subdomains"],
+                                            current_records=current_records,
+                                            ip=public_ip)
+        records_to_update = r_to_update + records_to_update
+    
+    for r in records_to_update:
+        result = update_record(url=porkbun_url, headers=headers, body=body,
+                               domain=r["domain"], subdomain=r["subdomain"],
+                               ip=public_ip)
+        print(r["domain"], r["subdomain"], result)
+    
 
 if __name__ == "__main__":
     main()
